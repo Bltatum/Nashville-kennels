2ebbf696686b710e15d5fc354951fc9199aeb30c
brennen <bmar566@gmail.com>
2020-04-20 15:47:41 -0500

    initial commit

---
+++ src/components/customer/Customer.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default () => (
+  <section className="customer">
+    <h3 className="customer__name">John</h3>
+    <div className="customer_address">Address: 1 main st.</div>
+  </section>
+);
---
+++ src/components/location/Location.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default () => (
+  <section className="location">
+    <h3 className="location_name">Nashville Kennels West</h3>
+    <div className="location_address">100 2nd ave N</div>
+  </section>
+);
